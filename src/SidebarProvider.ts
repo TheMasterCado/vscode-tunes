6a8ad63aa59734d0bcaf4cb580a0f1a45ee11a7c
TheMasterCado <themastercado@outlook.com>
2021-02-14 23:01:20 -0500

    add user view

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -52,6 +52,9 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
     const styleVSCodeUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
     );
+    const styleGlobalUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "global.css")
+    );
     const scriptUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
     );
@@ -74,8 +77,10 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="${styleResetUri}" rel="stylesheet">
         <link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${styleGlobalUri}" rel="stylesheet">
         <link href="${styleMainUri}" rel="stylesheet">
         <script nonce="${nonce}">
+          const tsvscode = acquireVsCodeApi();
         </script>
       </head>
       <body>
--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,6 +1,65 @@
+<div class="sidebar-container">
+  <ul class="no-bullets">
+    {#each users as user (user.uuid)}
+      <li class="user">
+        <p class="user-name">{user.name}</p> <div class="song-info-wrapper"><p class="song-info">{user.currentlyListening}</p></div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<script lang="ts">
+import { onMount } from "svelte";
+
+
+  let users = [
+    {uuid: '1', name: 'Alexis Cadorette', currentlyListening: 'Very long song name by a very long author name that played the song and sang it'}
+  ]
+
+  onMount(() => {
+    window.addEventListener("message", (event) => {
+      const message = event.data;
+    });
+  });
+</script>
 
 <style>
+  .sidebar-container {
+    padding: 10px 20px;
+  }
 
+  ul.no-bullets {
+    padding: 0;
+  }
+  ul.no-bullets li {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  .user-name {
+    font-weight: bold;
+    white-space: nowrap;
+    flex-grow: 1;
+    margin-right: 7px;
+  }
+  .user {
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: nowrap;
+  }
+  .song-info-wrapper {
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .song-info {
+    color: green;
+    animation: move 5s linear infinite
+  }
+
+  @keyframes move {
+    from { transform: translateX(100%); }
+    to { transform: translateX(-100%); }
+  }
 </style>
-
-<h1> Hello </h1>
