f6c1e8771a030783f3f64553e4e3bd9d01c94bc3
TheMasterCado <themastercado@outlook.com>
2021-03-17 02:18:34 -0400

    bump v1.3.1

--- extension/CHANGELOG.md
+++ extension/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## 1.3.1 (2021-03-17)
+
+- Fixed SSL bug with the axios library
+
 ## 1.3.0 (2021-03-17)
 
 - Added actual website, domain and SSL to the extension API
--- extension/src/vscodeTunesApi.ts
+++ extension/src/vscodeTunesApi.ts
@@ -1,6 +1,7 @@
 import { ExtensionState } from "./ExtensionState";
 
 import axios from "axios";
+import https from "https";
 import { apiBaseUrl } from "./constants";
 
 export const refreshSpotifyAccessToken = async () => {
@@ -8,12 +9,13 @@ export const refreshSpotifyAccessToken = async () => {
   if (!refreshToken) {
     throw new Error("Could not refresh Spotify access token");
   }
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
   const response = await axios.post(
     `${apiBaseUrl}/auth/spotify/refresh`,
     {
       refresh_token: refreshToken,
     },
-    { headers: { "Content-Type": "application/json" } }
+    { headers: { "Content-Type": "application/json" }, httpsAgent }
   );
   const newAccessToken: string = response.data.access_token;
   const newRefreshToken: string | undefined = response.data.refresh_token;
@@ -33,7 +35,7 @@ export const updateCurrentlyPlaying = async (currentlyPlaying: any) => {
   if (!accessToken) {
     throw new Error("Not logged in");
   }
-
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
   const response = await axios.patch(
     `${apiBaseUrl}/me`,
     {
@@ -44,6 +46,7 @@ export const updateCurrentlyPlaying = async (currentlyPlaying: any) => {
         authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
       },
+      httpsAgent,
     }
   );
   if (response.status !== 200) {
