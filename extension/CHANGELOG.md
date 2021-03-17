159cfb11cd6ca63276260b2c2fa9025a5999252c
TheMasterCado <themastercado@outlook.com>
2021-03-17 01:56:47 -0400

    v1.3 (added ssl, domain and actual website)

--- extension/CHANGELOG.md
+++ extension/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## 1.3 (2021-03-17)
+
+- Added actual website, domain and SSL to the extension API
+
 ## 1.2.4 (2021-03-05)
 
 - Fixed bug that caused user to relogin each time
--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -1,7 +1,7 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const apiBaseUrl = __prod__
-  ? "http://vscodetunes.themastercado.com"
+  ? "https://vscodetunes.themastercado.com"
   : "http://localhost:3002";
 export const apiWsUrl = __prod__
-  ? "ws://vscodetunes.themastercado.com/realtime"
+  ? "wss://vscodetunes.themastercado.com/realtime"
   : "ws://localhost:3002/realtime";
